207bbc4099227a9b103610bd3c7366619b49a109
MariaContu <mariaescontu@gmail.com>
2026-06-04 10:22:27 -0300

    refactor: clean state and improve card behavior

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,26 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Card } from '../../models/card.model';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   templateUrl: './card.html',
-  styleUrl: './card.scss'
+  styleUrl: './card.scss',
 })
-export class CardComponent {
-
+export class CardComponent implements OnChanges {
   @Input() card!: Card;
-  @Input() isReversed = false;
 
   isInverted = false;
+
+  ngOnChanges() {
+    this.isInverted = false;
+  }
 
   toggleInvert() {
     this.isInverted = !this.isInverted;
   }
 
   getMeanings() {
-    return this.isInverted
-      ? this.card.reversedMeanings
-      : this.card.meanings;
+    return this.isInverted ? this.card.reversedMeanings : this.card.meanings;
   }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { TarotService } from '../../services/tarot';
 import { Card } from '../../models/card.model';
 import { CardComponent } from '../../components/card/card';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -11,26 +10,16 @@ import { NgIf } from '@angular/common';
   templateUrl: './home.html',
 })
 export class HomeComponent {
-
-  isReversed = false;
-
-  selectedReading: 'general' | 'career' | 'health' | 'relationships' = 'general';
-
-  selectedArcana: 'all' | 'major' | 'minor' = 'all';
-
-  selectedSuit: 'all' | 'cups' | 'swords' | 'wands' | 'pentacles' = 'all';
-
   allCards: Card[] = [];
 
   selectedCard?: Card;
   showModal = false;
 
+  constructor(private tarotService: TarotService) {}
 
   ngOnInit() {
     this.allCards = this.tarotService.getAllCards();
   }
-
-  constructor(private tarotService: TarotService) {}
 
   openModal(card: Card) {
     this.selectedCard = card;
