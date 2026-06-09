71b1974f47fdf58d21fc4f189fd1887b1848de2d
mecontudo <mariaescontu@gmail.com>
2026-06-09 17:45:46 -0300

    feat: conecta filtros ao grid

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TarotService } from '../../services/tarot';
 import { Card } from '../../models/card.model';
 import { CardComponent } from '../../components/card/card';
+import { FiltersComponent } from '../../components/filters/filters';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, FiltersComponent],
   templateUrl: './home.html',
+  styleUrl: './home.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   allCards: Card[] = [];
+  filteredCards: Card[] = [];
 
   selectedCard?: Card;
   showModal = false;
+  activeReading: 'general' | 'career' | 'health' | 'relationships' = 'general';
 
   constructor(private tarotService: TarotService) {}
 
   ngOnInit() {
     this.allCards = this.tarotService.getAllCards();
+    this.filteredCards = this.allCards;
+  }
+
+  onFiltersChange(filters: { arcana: string; suit: string; reading: 'general' | 'career' | 'health' | 'relationships' }) {
+    this.activeReading = filters.reading;
+
+    this.filteredCards = this.allCards.filter(card => {
+      const matchArcana = filters.arcana === 'all' || card.arcana === filters.arcana;
+      const matchSuit = filters.suit === 'all' || card.suit === filters.suit;
+      return matchArcana && matchSuit;
+    });
   }
 
   openModal(card: Card) {
